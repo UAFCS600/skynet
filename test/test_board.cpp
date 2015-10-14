18cfdf837a653e7e2c37deee64ba33f6c357ee47
Bucky Frost <frozenouthere@gmail.com>
2015-10-13 20:25:32 -0800

    Fixing board layout.
    Position 0 is blank, followed by red pieces. #1

--- test/test_board.cpp
+++ test/test_board.cpp
@@ -35,14 +35,14 @@ TEST_CASE ("Helper Functions") {
 TEST_CASE ("Board Validation") {
 
 	ai::checkers_board_t initial_board =
-	"r_r_r_r_" //7  layer 0
-	"_r_r_r_r" //15 layer 1
-	"r_r_r_r_" //23   .
+	"_r_r_r_r" //7  layer 0
+	"r_r_r_r_" //15 layer 1
+	"_r_r_r_r" //23   .
 	"________" //31   .
 	"________" //39   .
-	"_b_b_b_b" //47
-	"b_b_b_b_" //55
-	"_b_b_b_b";//63 layer 7
+	"b_b_b_b_" //47
+	"_b_b_b_b" //55
+	"b_b_b_b_";//63 layer 7
 
 	// Would need to track a case where pieces are added invalidly
 	SECTION ("Board Validation") {
@@ -52,46 +52,46 @@ TEST_CASE ("Board Validation") {
 
 TEST_CASE ("Initial Moves") {
 	ai::checkers_board_t initial_board =
-	"r_r_r_r_" //7
-	"_r_r_r_r" //15
-	"r_r_r_r_" //23
+	"_r_r_r_r" //7
+	"r_r_r_r_" //15
+	"_r_r_r_r" //23
 	"________" //31
 	"________" //39
-	"_b_b_b_b" //47
-	"b_b_b_b_" //55
-	"_b_b_b_b";//63
+	"b_b_b_b_" //47
+	"_b_b_b_b" //55
+	"b_b_b_b_";//63
 
-	std::string red_states_layers01="r_r_r_r_"
-					"_r_r_r_r";
+	std::string red_states_layers01="_r_r_r_r"
+					"r_r_r_r_";
 
 	std::vector<std::string> red_states_layer2;
 	std::vector<std::string> red_states_layer3;
 
-	red_states_layer2.emplace_back("__r_r_r_");
-	red_states_layer3.emplace_back("_r______");
+	red_states_layer2.emplace_back("___r_r_r");
+	red_states_layer3.emplace_back("r_______");
 
-	red_states_layer2.emplace_back("r___r_r_");
-	red_states_layer3.emplace_back("_r______");
+	red_states_layer2.emplace_back("___r_r_r");
+	red_states_layer3.emplace_back("__r_____");
 
-	red_states_layer2.emplace_back("r___r_r_");
-	red_states_layer3.emplace_back("___r____");
+	red_states_layer2.emplace_back("_r___r_r");
+	red_states_layer3.emplace_back("__r_____");
 
-	red_states_layer2.emplace_back("r_r___r_");
-	red_states_layer3.emplace_back("___r____");
+	red_states_layer2.emplace_back("_r___r_r");
+	red_states_layer3.emplace_back("____r___");
 
-	red_states_layer2.emplace_back("r_r___r_");
-	red_states_layer3.emplace_back("_____r__");
+	red_states_layer2.emplace_back("_r_r___r");
+	red_states_layer3.emplace_back("____r___");
 
-	red_states_layer2.emplace_back("r_r_r___");
-	red_states_layer3.emplace_back("_____r__");
+	red_states_layer2.emplace_back("_r_r___r");
+	red_states_layer3.emplace_back("______r_");
 
-	red_states_layer2.emplace_back("r_r_r___");
-	red_states_layer3.emplace_back("_____r__");
+	red_states_layer2.emplace_back("_r_r_r__");
+	red_states_layer3.emplace_back("______r_");
 
 	std::string blk_state = "________"
-				"_b_b_b_b"
 				"b_b_b_b_"
-				"_b_b_b_b";
+				"_b_b_b_b"
+				"b_b_b_b_";
 
 	ai::checkers_player_t red = "r";
 	ai::checkers_board_list_t red_result = ai::move_generator(initial_board, red);

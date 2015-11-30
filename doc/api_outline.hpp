d5ef0ac70398c7bc5c70f44eaa708e04350f86b9
Mike Moss <mrmoss@alaska.edu>
2015-11-30 12:39:38 -0900

    Proposed new status interface for game backend...

--- doc/api_outline.hpp
+++ doc/api_outline.hpp
@@ -34,11 +34,22 @@ namespace skynet
 
 	checkers_player_t checkers_player_from_string(const std::string& str);
 
-	typedef std::string checkers_status_t;
+	enum checkers_status_t
+	{
+		RED_TURN,
+		BLACK_TURN,
+		RED_WON,
+		BLACK_WON
+	};
+
+	checkers_status_t checkers_status_from_string(const std::string& str);
+
 	typedef std::string checkers_board_t;
 	typedef std::vector<checkers_board_t> checkers_board_list_t;
-	void validate(const checkers_board_t& board); //Throws on bad board
-	checkers_board_list_t move_generator(const checkers_board_t& board,const checkers_player_t& player); //Throws on bad board
+
+	void validate(const checkers_board_t& board);
+
+	checkers_board_list_t move_generator(const checkers_board_t& board,const checkers_player_t& player);
 
 	//Alpha Beta
 	class alpha_beta_t
@@ -73,7 +84,8 @@ namespace skynet
 
 namespace std
 {
-	std::string to_string(skynet::checkers_player_t player);
+	std::string to_string(const skynet::checkers_player_t& player);
+	std::string to_string(const skynet::checkers_board_list_t& boards);
 }
 
 #endif

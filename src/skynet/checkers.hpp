67c6e3597a4a495195f4d123dd2321c36004de8e
Mike Moss <mrmoss@alaska.edu>
2015-12-18 16:16:47 -0900

    Added documentation.

--- src/skynet/checkers.hpp
+++ src/skynet/checkers.hpp
@@ -1,3 +1,5 @@
+/*! @file */
+
 #ifndef CHECKERS_HPP
 #define CHECKERS_HPP
 
@@ -6,36 +8,71 @@
 
 namespace skynet
 {
+	//! Enum for determining the checkers player (used primarily for making moves).
 	enum checkers_player_t
 	{
-		RED,
-		BLACK
+		RED,			/**<Red player.*/
+		BLACK			/**<Black player.*/
 	};
 
+	/**String to checkers player conversion function.
+	\param str			String representing the checkers player ("red" or "black" - case insensitive).
+	\return 			Enum for indicated player.
+	*/
 	checkers_player_t checkers_player_from_string(const std::string& str);
 
+	//!Enum for determing the status of a checkers game.
 	enum checkers_status_t
 	{
-		RED_TURN,
-		BLACK_TURN,
-		RED_WON,
-		BLACK_WON
+		RED_TURN,		/**<Red player's turn.*/
+		BLACK_TURN,		/**<Black player's turn.*/
+		RED_WON,		/**<Red player has won or black player has lost (ex: invalid move).*/
+		BLACK_WON		/**<Black player has won or red player has lost (ex: invalid move).*/
 	};
 
+	/**String to checkers status conversion function.
+	\param str			String representing the checkers status ("red_turn", "black_turn", "red_won", or "black_won" - case insensitive).
+	\return 			Enum for indicated status.
+	*/
 	checkers_status_t checkers_status_from_string(const std::string& str);
 
+	//!Checkers board type containing 'r' for a red pawn, 'R' for a red king, 'b' for a black pawn, 'B' for a black king, and '_' for an empty square.
 	typedef std::string checkers_board_t;
+
+	//!Checkers board list type (vector of checkers boards).
 	typedef std::vector<checkers_board_t> checkers_board_list_t;
 
+	/**Validation function, throws std::runtime_error if passed board is invalid (in terms of format).
+	\param board		String representation of the checkers board (ex: "rrrrrrrrrrrr________bbbbbbbbbbbb").
+	*/
 	void validate(const checkers_board_t& board);
 
+	/**Generates moves for a given checkers board (throws std::runtime_error on boards with invalid formatting).
+	\param board		String representation of the checkers board (ex: "rrrrrrrrrrrr________bbbbbbbbbbbb").
+	\param player		Player to find moves for.
+	\return				List of possibly moves for the given player from the given board.
+	*/
 	checkers_board_list_t move_generator(const checkers_board_t& board,const checkers_player_t& player);
 }
 
 namespace std
 {
+	/**Checkers player to string function.
+	\param player		Player to convert.
+	\return 			String value of the enum typed player.
+	*/
 	std::string to_string(const skynet::checkers_player_t& player);
+
+	/**Checkers status to string function.
+	\param status		Status to convert.
+	\return 			String value of the enum typed status.
+	*/
 	std::string to_string(const skynet::checkers_status_t& status);
+
+	/**Checkers board list to string function.
+	\param player		List of checkers boards to convert.
+	\return 			JSON formatted array of checkers boards.
+	*/
 	std::string to_string(const skynet::checkers_board_list_t& boards);
 }
 
--- src/skynet/neuralnet.hpp
+++ src/skynet/neuralnet.hpp
@@ -1,3 +1,5 @@
+/*! \file */
+
 #ifndef NEURALNET_HPP
 #define NEURALNET_HPP
 
@@ -6,20 +8,39 @@
 
 namespace skynet
 {
+	/**
+	\class neuralnet_t
+	*Stores and performs evaluations for a standard feed forward neural network in the form of weights.
+	*/
 	class neuralnet_t
 	{
-		struct node_t
-		{
-			std::vector<double> weights;
-			double value;
-		};
-
 		public:
+			/**
+			*Will throw std::runtime_error on bad input.
+			\param layers				A vector of size_t stating the layer layout (blondie24 ex: {32,24,16,1}).
+			\param weights				A vector of double that stores the weights for the neural network.
+			*/
 			neuralnet_t(const std::vector<size_t>& layers,const std::vector<double>& weights);
+
+
+			/**
+			*Will throw std::runtime_error on sigmoid index or invalid constants.
+			\param inputs 				Layer 0 input values to perform evalation on.
+			\param sigmoid_index 		A value of 0 indicates use y=x and 1 indicates use y=a/(1+e^(-x/b))+c;
+			\param a					Index 1 constant.
+			\param b					Index 1 constant (cannot be 0).
+			\param c					Index 1 constant.
+			*/
 			double evaluate(const std::vector<double>& inputs,const size_t sigmoid_index,
 				const double a=1,const double b=1,const double c=0);
 
 		private:
+			struct node_t
+			{
+				std::vector<double> weights;
+				double value;
+			};
+
 			void create_node_layers_m();
 			void set_node_weights_m(const std::vector<double>& weights);
 

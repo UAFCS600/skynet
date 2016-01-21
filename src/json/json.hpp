604287b6283fc397a25f3f5f93c0d12841a9a4dd
Mike Moss <mrmoss@alaska.edu>
2016-01-20 15:57:10 -0900

    Site update and documentation update.

--- src/json/json.hpp
+++ src/json/json.hpp
@@ -1,3 +1,5 @@
+/*! @file */
+
 #ifndef JSON_HPP
 #define JSON_HPP
 
@@ -5,14 +7,43 @@
 #include <string>
 #include <vector>
 
+//!Typedef'd in-case JSON library is ever changed.
 typedef Json::Value json_t;
 
+/**Converts a json_t to a std::string (same as the std::to_string function).
+\param json			JSON object to convert.
+\return				String representation of JSON object.
+*/
 std::string JSON_serialize(const json_t& json);
+
+/**Converts a std::string represented JSON object into a json_t (throws on bad JSON string).
+\param stringified	String representation of JSON object.
+\return				JSON object of the passed string.
+*/
 json_t JSON_parse(const std::string& stringified);
 
+/**Converts a json_t to a std::string (same as the std::string JSON_serialize function).
+\param json			JSON object to convert.
+\return				String representation of JSON object.
+*/
 std::string to_string(const json_t& json);
+
+/**Converts a JSON array object containing doubles or ints to a C++ vector of doubles (throws if json array contains values other than doubles or ints).
+\param json			JSON object to convert.
+\return				Vector of doubles.
+*/
 std::vector<double> to_double_array(const json_t& json);
+
+/**Converts a JSON array object containing unsigned ints to a C++ vector of size_t (throws if json array contains values other than ints >= 0).
+\param json			JSON object to convert.
+\return				Vector of size_t.
+*/
 std::vector<size_t> to_size_array(const json_t& json);
+
+/**Converts a JSON array object containing JSON array objects of doubles or ints to a C++ vector of vector of doubles (throws if json array contains values that contain values other than doubles or ints).
+\param json			JSON object to convert.
+\return				Vector of vector of doubles.
+*/
 std::vector<std::vector<double>> to_array_double_array(const json_t& json);
 
 #endif
--- src/skynet/checkers_client.hpp
+++ src/skynet/checkers_client.hpp
@@ -1,3 +1,5 @@
+/*! @file */
+
 #ifndef CHECKERS_CLIENT_HPP
 #define CHECKERS_CLIENT_HPP
 
@@ -10,27 +12,59 @@ namespace skynet
 {
 	namespace checkers
 	{
+		/**
+		\struct game_info_t
+		*Stores game information.
+		*/
 		struct game_info_t
 		{
+			//!Status of the game.
 			skynet::checkers::status_t status;
+			//!All boards played during the game (beginning with the initial board).
 			skynet::checkers::board_list_t boards;
+			//!Time of game creation (in seconds since the Unix epoch).
 			uint64_t create_time;
+			//!Last time game was modified (such as a board was played, in seconds since the Unix epoch).
 			uint64_t modify_time;
 		};
 
+		//!Game list map type (key being the game name and value being the info for that game).
 		typedef std::map<std::string,game_info_t> game_list_t;
 
+		/**Downloads information for all checkers games on a given server (throws on error).
+		\param server		DNS name or IP address of the hosting server (ex: "skynet.cs.uaf.edu" or "137.229.25.135").
+		\return				Map of games.
+		*/
 		game_list_t list_games(const std::string& server);
-		game_info_t info_game(const std::string& server,
-			const std::string& game_name);
-		void play_game(const std::string& server,
-			const std::string& game_name,const skynet::checkers::board_t& board);
+
+		/**Downloads information for a specific checkers game on a given server (throws on error).
+		\param server		DNS name or IP address of the hosting server (ex: "skynet.cs.uaf.edu" or "137.229.25.135").
+		\param game_name	Name of the game to get info for.
+		\return				Map of games.
+		*/
+		game_info_t info_game(const std::string& server,const std::string& game_name);
+
+		/**Places a board (makes a move) for a given server and game name. (throws on error).
+		\param server		DNS name or IP address of the hosting server (ex: "skynet.cs.uaf.edu" or "137.229.25.135").
+		\param game_name	Name of the game to make a move on.
+		\param board		Board to play.
+		*/
+		void play_game(const std::string& server,const std::string& game_name,const skynet::checkers::board_t& board);
 	}
 }
 
 namespace std
 {
+	/**Converts a map of games to a JSON string.
+	\param list				Map of game names to game info structs.
+	\return					Stringified JSON object.
+	*/
 	std::string to_string(const skynet::checkers::game_list_t& list);
+
+	/**Converts a game info object to a JSON string.
+	\param info				Game info struct.
+	\return					Stringified JSON object.
+	*/
 	std::string to_string(const skynet::checkers::game_info_t& info);
 }
 
